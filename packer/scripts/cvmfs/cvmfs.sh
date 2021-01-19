dff64abe27460e0b8dcb6b3348fc602c216a671a
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-01-19 10:31:02 -0700

    Add CVMFS bootstrapper and config

---
+++ packer/scripts/cvmfs/cvmfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+set -ex
+
+# Install CVMFS acording to official docs
+wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+sudo dpkg -i cvmfs-release-latest_all.deb
+rm -f cvmfs-release-latest_all.deb
+sudo apt-get update -y
+sudo apt-get install -y cvmfs
+
+# Ensure AutoFS is working
+sudo cvmfs_config setup
+
+# The configuration will be done on deployment so that we can populate
+# the proxy addresses
