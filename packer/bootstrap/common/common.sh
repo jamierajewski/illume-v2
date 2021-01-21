b5579193d0c3eb453444036ccc1f308d19e46ad3
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-01-21 11:20:34 -0700

    Add custom profile for system wide configs

--- packer/bootstrap/common/common.sh
+++ packer/bootstrap/common/common.sh
@@ -15,6 +15,10 @@ sudo sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no\n/g' /etc/ssh
 sudo sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no\n/g' /etc/ssh/sshd_config
 sudo sed -i 's/^GSSAPIAuthentication.*/GSSAPIAuthentication no\n/g' /etc/ssh/sshd_config
 
+# Create system-wide profile override for custom additions
+echo "#!/bin/bash" | sudo tee /etc/profile.d/custom.sh > /dev/null
+sudo chmod +x /etc/profile.d/custom.sh
+
 # Enable rpc service
 sudo systemctl add-wants multi-user.target rpc-statd.service
 
