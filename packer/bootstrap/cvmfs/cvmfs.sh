cdd38cd4129c3fe6275da3d14e3524a328453a3a
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-01-26 15:00:34 -0700

    Add deps for sft.cern.ch to cvmfs

--- packer/bootstrap/cvmfs/cvmfs.sh
+++ packer/bootstrap/cvmfs/cvmfs.sh
@@ -5,7 +5,10 @@ set -ex
 # Install CVMFS acording to official docs
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
-rm -f cvmfs-release-latest_all.deb
+# Install dependencies for sft.cern.ch repo
+wget https://gitlab.cern.ch/linuxsupport/rpms/HEP_OSlibs/raw/8.0.0-1.focal/downloads/heposlibs_8.0.0-1.focal_amd64.deb
+sudo apt-get install ./heposlibs_8.0.0-1.focal_amd64.deb
+rm -f cvmfs-release-latest_all.deb heposlibs_8.0.0-1.focal_amd64.deb
 sudo apt-get update -y
 sudo apt-get install -y cvmfs
 
