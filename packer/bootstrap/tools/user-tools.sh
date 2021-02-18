685e10502a0c3b82b11629c75d8fb7d961473ea3
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-02-18 13:40:55 -0700

    Add user tools

--- packer/bootstrap/tools/user-tools.sh
+++ packer/bootstrap/tools/user-tools.sh
@@ -3,4 +3,39 @@
 set -ex
 
 sudo apt-get update
-sudo apt-get install -y tmux emacs
+
+# Build tools and utilities
+sudo apt-get install -y build-essential \
+    vim emacs \
+    zsh tcsh \
+    postgresql mariadb-server sqlite3 \
+    jq valgrind subversion htop \
+    default-jre default-jdk
+
+# Golang already installed in singularity.sh
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust-setup.sh
+chmod +x rust-setup.sh
+./rust-setup.sh -y
+rm -f rust-setup.sh
+
+# Anaconda - includes all scientific tools that users may wish to use,
+# and can create both python2 and 3 environments
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh -O ~/anaconda.sh
+sudo bash ~/anaconda.sh -b -p /opt/anaconda3
+sudo chmod 755 -R /opt/anaconda3/
+echo 'export PATH=$PATH:/opt/anaconda3/bin' | sudo tee -a /etc/profile.d/custom.sh > /dev/null
+rm -f ~/anaconda.sh
+
+# Python 3
+sudo apt-get install -y python3-dev python3-pip
+
+# Python 2, libs, tools
+sudo apt-get install -y python2 python2-dev
+# Need to get pip manually
+curl https://bootstrap.pypa.io/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+rm -f get-pip.py
+
+sudo reboot
