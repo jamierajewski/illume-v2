167ea1dcd7a1329465c27cef1a7bb9851d89406f
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-05-07 17:04:26 -0600

    Install stable podman, not testing

--- packer/bootstrap/podman/podman.sh
+++ packer/bootstrap/podman/podman.sh
@@ -4,7 +4,8 @@ set -ex
 
 # Install podman according to the official docs 
 . /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get -y install podman
