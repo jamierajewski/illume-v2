f292bc6c007299982be8b444d2d612745019572b
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-06-14 12:44:44 -0600

    Create monitor volume in advance and attach

--- terraform/hosts-monitor.tf
+++ terraform/hosts-monitor.tf
@@ -5,6 +5,12 @@ data "openstack_images_image_v2" "monitor-image" {
   most_recent = true
 }
 
+resource "openstack_blockstorage_volume_v3" "monitor-volume" {
+  name = "monitor-volume"
+  size = "30"
+  image_id = data.openstack_images_image_v2.monitor-image.id
+}
+
 resource "openstack_compute_instance_v2" "illume-monitor-v2" {
   name = "illume-monitor-v2"
   flavor_name     = "c2-4GB-45"
@@ -13,12 +19,11 @@ resource "openstack_compute_instance_v2" "illume-monitor-v2" {
 
   # Boot from volume (created from image)
   block_device {
-    uuid                  = data.openstack_images_image_v2.monitor-image.id
-    source_type           = "image"
-    volume_size           = 30
+    uuid                  = openstack_blockstorage_volume_v3.monitor-volume.id
+    source_type           = "volume"
     boot_index            = 0
     destination_type      = "volume"
-    delete_on_termination = false
+    delete_on_termination = true
   }
 
   # Assign all ephemeral storage for this flavor (45GB),
