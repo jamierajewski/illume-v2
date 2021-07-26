7c5fcf97aa759dd3a47c1105ac0dc1e601ff3ae3
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-07-26 11:17:15 -0600

    Add ability to build up to specific stage with packer build script

--- packer/vm-profiles/build-all.sh
+++ packer/vm-profiles/build-all.sh
@@ -1,45 +1,62 @@
-#!/bin/bash -l
+#!/bin/bash
 
-set -ex
+set -e
 
 printf "\t\t\t\t\t[ Illume v2 - Builder CLI ]\n"
 
+STAGES=8
+if [[ $# -eq 1 && $1 -gt 0 && $1 -le $STAGES ]]; then
+    STAGES=$1
+fi
 
-# cd base
-# printf "\n\n[ BUILDING NON-INTERACTIVE ]\n\n"
-# packer build -force non-interactive.pkr.hcl
-# printf "\n\n[ BUILDING INTERACTIVE ]\n\n"
-# packer build -force interactive.pkr.hcl
+# Non-interactive and interactive
+if [[ $STAGES -ge 1 ]]; then
+    cd base
+    packer build -force non-interactive.pkr.hcl
+    packer build -force interactive.pkr.hcl
+    cd ..
+fi
 
-# cd ../bastion
-# printf "\n\n[ BUILDING BASTION ]\n\n"
-# packer build -force bastion.pkr.hcl
+if [[ $STAGES -ge 2 ]]; then
+    cd bastion
+    packer build -force bastion.pkr.hcl
+    cd ..
+fi
 
-# cd ../ingress
-# printf "\n\n[ BUILDING INGRESS ]\n\n"
-# packer build -force ingress.pkr.hcl
+if [[ $STAGES -ge 3 ]]; then
+    cd ingress
+    packer build -force ingress.pkr.hcl
+    cd ..
+fi
 
-# cd ../monitor
-# printf "\n\n[ BUILDING MONITOR ]\n\n"
-# packer build -force monitor.pkr.hcl
+if [[ $STAGES -ge 4 ]]; then
+    cd monitor
+    packer build -force monitor.pkr.hcl
+    cd ..
+fi
 
-# cd ../ldap
-# printf "\n\n[ BUILDING openLDAP ]\n\n"
-# packer build -force openLDAP.pkr.hcl
-# printf "\n\n[ BUILDING phpLDAPadmin ]\n\n"
-# packer build -force phpLDAPadmin.pkr.hcl
+if [[ $STAGES -ge 5 ]]; then
+    cd ldap
+    packer build -force openLDAP.pkr.hcl
+    packer build -force phpLDAPadmin.pkr.hcl
+    cd ..
+fi
 
-# cd ../proxy
-# printf "\n\n[ BUILDING PROXY ]\n\n"
-# packer build -force proxy.pkr.hcl
+if [[ $STAGES -ge 6 ]]; then
+    cd proxy
+    packer build -force proxy.pkr.hcl
+    cd ..
+fi
 
-# cd ../control
-# printf "\n\n[ BUILDING CONTROL ]\n\n"
-# packer build -force control.pkr.hcl
+if [[ $STAGES -ge 7 ]]; then
+    cd control
+    packer build -force control.pkr.hcl
+    cd ..
+fi
 
-# cd ../worker
-cd worker
-printf "\n\n[ BUILDING WORKER-NOGPU ]\n\n"
-packer build -force worker-nogpu.pkr.hcl
-printf "\n\n[ BUILDING WORKER-GPU ]\n\n"
-packer build -force worker-gpu.pkr.hcl
+if [[ $STAGES -eq 8 ]]; then
+    cd worker
+    packer build -force worker-gpu.pkr.hcl
+    packer build -force worker-nogpu.pkr.hcl
+    cd ..
+fi
